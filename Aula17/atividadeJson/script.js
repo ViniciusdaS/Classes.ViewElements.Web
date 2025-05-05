a335445d8086ac9327097177f379acb6205e4452
Astro_Dev <oliveirateixeira653@gmail.com>
2025-05-05 17:28:22 -0300

    Add files via upload

---
+++ Aula17/atividadeJson/script.js
@@ -0,0 +1,15 @@
+fetch('objects.json') 
+
+.then(response => response.json()) 
+
+.then(data => {
+    const objectList = document.getElementById('object-list');
+
+    data.objects.foreach(obj => {
+        const listItem = document.createElement('li');
+        listItem.textContent = obj;
+        objectList.appendChild(listItem);
+    }) 
+})
+
+.catch(error => console.error('Erro ao carregar o arquivo JSON: ', error));
