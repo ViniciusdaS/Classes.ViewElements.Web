098c4f25c21e69d971ee5016937befcc022c18f9
Astro_Dev <oliveirateixeira653@gmail.com>
2025-05-08 17:27:11 -0300

    Add files via upload

--- aula18/Pokedex/script.js
+++ aula18/Pokedex/script.js
@@ -10,58 +10,60 @@ let peso = document.querySelector("#peso");
 let altura = document.querySelector("#altura");
 let btnBack = document.querySelector("#btnVoltar");
 let btnProx = document.querySelector("#idProx");
-let somPoke = document.querySelector("#somPoke");
+let audioPoke = document.querySelector("#audioPoke");
 
-let numeroPokedex = 1; 
+let numeroPokedex = 1000; 
+
+/*const audio = new Audio(dados.sprites.front_default);
+audio.play();*/
 
 const fetchPokemon = async(pokemon) => {
     const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     const data = await APIresponse.json();
     return data;  
-}
+};
 
-fetch('seu_arquivo.mp3')
-.then(response => response.arrayBuffer())
-.then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
-.then(audioBuffer => {
-    const source = audioContext.createBufferSource();
-    source.buffer = audioBuffer;
-    // Conectar a fonte ao destino (geralmente o destino do contexto)
-    source.connect(audioContext.destination);
-    // Iniciar a reprodução
-    source.start();
-})
 
 const showPokemon = async(pokemon) =>{
     const dataPokemon = await fetchPokemon(pokemon);
-    imgPokemon.src = dataPokemon.sprites.front_default;
+    segundaImagem(dataPokemon.sprites.other.showdown.front_default,dataPokemon.sprites.front_default);
     nomePoke.innerHTML = dataPokemon.name; 
-    idPoke = dataPokemon.id;
-    tipo1Poke = dataPokemon.types[0].type.name;  
-    tipo2Poke = dataPokemon.types[1].type.name;
-    habilidade = dataPokemon.abilities[0].ability.name; 
-    peso = dataPokemon.weight;
-    altura = dataPokemon.height;
-}
+    idPoke.innerHTML = dataPokemon.id;
+    tipo1Poke.innerHTML = dataPokemon.types[0].type.name;  
+    tipo2Poke.innerHTML = dataPokemon.types[1].type.name;
+    habilidade.innerHTML = dataPokemon.abilities[0].ability.name; 
+    peso.innerHTML = dataPokemon.weight / 10 + " kg";
+    altura.innerHTML = dataPokemon.height / 10 + " m";
+    audioPoke.innerHTML = dataPokemon.sprites.front_default; 
+};
 
 form.addEventListener("submit", (event) =>{
     event.preventDefault();
     showPokemon(inputF.value.toLowerCase());
-})
+});
 
 
 btnBack.addEventListener("click", (event) => {
-    if(numeroPokedex > 1)
+    if(numeroPokedex > 0)
     {
         numeroPokedex = numeroPokedex - 1
     }
     showPokemon(numeroPokedex);
-})
+});
 
-next.addEventListener("click", (event) =>{
-    if(numeroPokedex < 1000)
+btnProx.addEventListener("click", (event) =>{
+    if(numeroPokedex > 0)
     {
-        numeroPokedex = numeroPokedex - 1
+        numeroPokedex = numeroPokedex + 1
     }
     showPokemon(numeroPokedex); 
-})
+});
+
+function segundaImagem(gif, image){
+    imgPokemon.src = gif;
+    imgPokemon.onerror = function(){
+        this.onerror = null; 
+        this.src = image; 
+    }
+    return
+}
